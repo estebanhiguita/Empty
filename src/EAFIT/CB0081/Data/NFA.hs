91f4c9f07d3e0236aff24e8834d6ffee17670b06
agomezl <alegomez544@gmail.com>
2013-07-16 18:28:16 -0400

    corrections

--- src/EAFIT/CB0081/Data/NFA.hs
+++ src/EAFIT/CB0081/Data/NFA.hs
@@ -2,11 +2,11 @@ module EAFIT.CB0081.Data.NFA where
 
 import Data.Set
 
--- | A deterministic finite automaton.
+-- | A non-deterministic finite automaton.
 data NFA state symbol =
   MkNFA { statesNFA  :: Set state                    -- ^ States
         , symbolsNFA :: Set symbol                   -- ^ Input symbols
         , deltaNFA   :: state -> symbol -> Set state -- ^ Transition function
         , startNFA   :: state                        -- ^ Start state
-        , finalsNFA  :: Set state                    -- ^ Final or accepting states
+        , finalsNFA  :: Set state                    -- ^ Final state
         }
