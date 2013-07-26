fef810d9c1b8b436378af833ca58a31633e88c91
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2013-07-26 01:53:14 -0500

    Hlint suggestions.

--- src/EAFIT/CB0081/Data/NFA/Examples.hs
+++ src/EAFIT/CB0081/Data/NFA/Examples.hs
@@ -3,54 +3,39 @@ module EAFIT.CB0081.Data.NFA.Examples where
 import EAFIT.CB0081.Data.NFA ( NFA(MkNFA) )
 import Data.Set              ( fromList, Set, singleton )
 
+states :: Set Int
+states = fromList [0,1,2,3,4]
+
+symbols :: Set Char
+symbols = fromList "ab"
+
+initialState :: Int
+initialState = 0
+
+finalStates :: Set Int
+finalStates = fromList [3]
+
+errorState :: Set Int
+errorState = singleton 5
+
 nonemptyNFA :: NFA Int Char
-nonemptyNFA = MkNFA states symbols delta initial_state final_states
+nonemptyNFA = MkNFA states symbols delta initialState finalStates
   where
-    states :: Set Int
-    states = fromList [0,1,2,3,4]
-
-    symbols :: Set Char
-    symbols = fromList ['a', 'b']
-
-    initial_state :: Int
-    initial_state = 0
-
-    final_states :: Set Int
-    final_states = fromList [3]
-
     delta :: Int -> Char -> Set Int
     delta 0 'a' = singleton 1
     delta 1 'b' = singleton 2
     delta 2 'a' = fromList [1,2]
     delta 3 'a' = singleton 4
     delta 2 'b' = fromList [3]
-    delta _ _   = error_state
-
-    error_state :: Set Int
-    error_state = singleton 5
+    delta _ _   = errorState
 
 emptyNFA :: NFA Int Char
-emptyNFA = MkNFA states symbols delta initial_state final_states
+emptyNFA = MkNFA states symbols delta initialState finalStates
   where
-    states :: Set Int
-    states = fromList [0,1,2,3,4]
-
-    symbols :: Set Char
-    symbols = fromList ['a', 'b']
-
-    initial_state :: Int
-    initial_state = 0
-
-    final_states :: Set Int
-    final_states = fromList [3]
-
     delta :: Int -> Char -> Set Int
     delta 0 'a' = singleton 1
     delta 1 'b' = singleton 2
     delta 2 'a' = fromList [1,2]
     delta 3 'a' = singleton 4
     delta 2 'b' = fromList [4]
-    delta _ _   = error_state
-
-    error_state :: Set Int
-    error_state = singleton 5
+    delta _ _   = errorState
